38db9063a7f5f724c360f5e6cca0a70fe4e4c30b
Jacob Weisz <ocdtrekkie@gmail.com>
2015-11-19 21:07:53 -0600

    Add Disable function to Speech module, make it Async

--- modSpeech.vb
+++ modSpeech.vb
@@ -1,8 +1,17 @@
 ﻿Module modSpeech
     Dim synth As System.Speech.Synthesis.SpeechSynthesizer
 
+    Sub Disable()
+        My.Application.Log.WriteEntry("Unloading speech module")
+        synth.Dispose()
+        My.Settings.Speech_Enable = False
+        My.Application.Log.WriteEntry("Speech module is disabled")
+    End Sub
+
     Sub Enable()
         My.Settings.Speech_Enable = True
+        My.Application.Log.WriteEntry("Speech module is enabled")
+        My.Application.Log.WriteEntry("Loading speech module")
         Load()
     End Sub
 
@@ -11,13 +20,13 @@
             synth = New Speech.Synthesis.SpeechSynthesizer
             synth.SetOutputToDefaultAudioDevice()
         Else
-            My.Application.Log.WriteEntry("Speech module is disabled")
+            My.Application.Log.WriteEntry("Speech module is disabled, module not loaded")
         End If
     End Sub
 
     Sub Say(ByVal TextToSpeak As String)
         If My.Settings.Speech_Enable = True Then
-            synth.Speak(TextToSpeak)
+            synth.SpeakAsync(TextToSpeak)
         End If
     End Sub
 End Module

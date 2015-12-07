3dd71e54449e23c4ea3c5e788e9b316ce56fc41a
Jacob Weisz <ocdtrekkie@gmail.com>
2015-12-07 01:14:59 -0600

    Don't ping if ping is disabled

--- modPing.vb
+++ modPing.vb
@@ -39,33 +39,37 @@ Module modPing
     End Sub
 
     Public Function Ping(ByVal host As String, Optional ByVal repeat As Integer = 1) As String
-        Try
-            Dim a As New System.Net.NetworkInformation.Ping
-            Dim b As System.Net.NetworkInformation.PingReply
-            Dim txtlog As String = ""
-            Dim c As New System.Net.NetworkInformation.PingOptions
-            c.DontFragment = True
-            c.Ttl = 64
-            Dim data As String = "aaaaaaaaaaaaaaaa"
-            Dim bt As Byte() = System.Text.Encoding.ASCII.GetBytes(data)
-            Dim i As Int16
-            For i = 1 To repeat
-                b = a.Send(host, 2000, bt, c)
-                If b.Status = Net.NetworkInformation.IPStatus.Success Then
-                    txtlog += "Reply from " & host & " in " & b.RoundtripTime & " ms, ttl " & b.Options.Ttl & vbCrLf
-                End If
-                If b.Status = Net.NetworkInformation.IPStatus.DestinationHostUnreachable Then
-                    txtlog += "Destination Host Unreachable" & vbCrLf
-                End If
-                If b.Status = Net.NetworkInformation.IPStatus.TimedOut Then
-                    txtlog += "Reply timed out" & vbCrLf
-                End If
-            Next i
-            Return txtlog
-        Catch ex As Exception
-            My.Application.Log.WriteException(ex)
-            Return "Ping error"
-        End Try
+        If My.Settings.Ping_Enable = True Then
+            Try
+                Dim a As New System.Net.NetworkInformation.Ping
+                Dim b As System.Net.NetworkInformation.PingReply
+                Dim txtlog As String = ""
+                Dim c As New System.Net.NetworkInformation.PingOptions
+                c.DontFragment = True
+                c.Ttl = 64
+                Dim data As String = "aaaaaaaaaaaaaaaa"
+                Dim bt As Byte() = System.Text.Encoding.ASCII.GetBytes(data)
+                Dim i As Int16
+                For i = 1 To repeat
+                    b = a.Send(host, 2000, bt, c)
+                    If b.Status = Net.NetworkInformation.IPStatus.Success Then
+                        txtlog += "Reply from " & host & " in " & b.RoundtripTime & " ms, ttl " & b.Options.Ttl & vbCrLf
+                    End If
+                    If b.Status = Net.NetworkInformation.IPStatus.DestinationHostUnreachable Then
+                        txtlog += "Destination Host Unreachable" & vbCrLf
+                    End If
+                    If b.Status = Net.NetworkInformation.IPStatus.TimedOut Then
+                        txtlog += "Reply timed out" & vbCrLf
+                    End If
+                Next i
+                Return txtlog
+            Catch ex As Exception
+                My.Application.Log.WriteException(ex)
+                Return "Ping error"
+            End Try
+        Else
+            Return "Ping disabled"
+        End If
     End Function
 
     Public Class CheckInternetConnectivity : Implements IJob

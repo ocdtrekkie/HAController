da7f84acf56621f63a19ac9c888e7a3b7bab5356
Jacob Weisz <ocdtrekkie@gmail.com>
2015-12-10 21:58:05 -0600

    Start assembling mail module for notifications
    Figuring out credentials will be a pain. Doesn't do much yet.

---
+++ modMail.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Net.Mail
+
+Module modMail
+    Public SmtpClient As SmtpClient = New SmtpClient
+
+    Sub Disable()
+        My.Application.Log.WriteEntry("Unloading mail module")
+        Unload()
+        My.Settings.Mail_Enable = False
+        My.Application.Log.WriteEntry("Mail module is disabled")
+    End Sub
+
+    Sub Enable()
+        My.Settings.Mail_Enable = True
+        My.Application.Log.WriteEntry("Mail module is enabled")
+        My.Application.Log.WriteEntry("Loading mail module")
+        Load()
+    End Sub
+
+    Sub Load()
+        SmtpClient.EnableSsl = True
+        SmtpClient.Host = My.Settings.Mail_Host
+        SmtpClient.Port = My.Settings.Mail_Port
+    End Sub
+
+    Sub Unload()
+
+    End Sub
+End Module

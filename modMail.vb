72849cbecdde3562024658cd2be790f94d5c5b20
Jacob Weisz <ocdtrekkie@gmail.com>
2016-02-08 00:39:47 -0600

    First part of mail checking code
    Kinda amazed I could find only one code sample that worked with SSL!
    This just gets the messages count so far, and logs them.

--- modMail.vb
+++ modMail.vb
@@ -1,8 +1,61 @@
-﻿Imports System.Net
+﻿Imports System.IO
+Imports System.Net
 Imports System.Net.Mail
+Imports System.Net.Security
+Imports System.Net.Sockets
+Imports System.Text
 
 Module modMail
-    Public oClient As SmtpClient = New SmtpClient
+    ' Credit for the POP3 part of the mail module goes to evry1falls from CodeProject: http://www.codeproject.com/Tips/441809/Receiving-response-from-POP3-mail-server
+
+    Private oClient As SmtpClient = New SmtpClient
+    Private pClient As TcpClient = New TcpClient
+    Private Read_Stream As StreamReader
+    Private NetworkS_tream As NetworkStream
+    Private m_sslStream As SslStream
+    Private server_Command As String
+    Private ret_Val As Integer
+    Private Response As String
+    Private Parts() As String
+    Private m_buffer() As Byte
+    Private StatResp As String
+    Private server_Stat(2) As String
+
+    Sub CheckMail()
+        If pClient.Connected = True Then
+            CloseServer()
+            pClient = New TcpClient(My.Settings.Mail_POPHost, My.Settings.Mail_POPPort)
+            ret_Val = 0
+            Exit Sub
+        Else
+            pClient = New TcpClient(My.Settings.Mail_POPHost, My.Settings.Mail_POPPort)
+
+            NetworkS_tream = pClient.GetStream 'Read the stream
+            m_sslStream = New SslStream(NetworkS_tream) 'Read SSL stream
+            m_sslStream.AuthenticateAsClient(My.Settings.Mail_POPHost) 'Auth
+            Read_Stream = New StreamReader(m_sslStream) 'Read the stream
+            StatResp = Read_Stream.ReadLine()
+
+            StatResp = Login(m_sslStream, "USER " & My.Settings.Mail_Username)
+            My.Application.Log.WriteEntry("POP3: " & StatResp)
+            StatResp = Login(m_sslStream, "PASS " & My.Settings.Mail_Password)
+            My.Application.Log.WriteEntry("POP3: " & StatResp)
+            StatResp = Login(m_sslStream, "STAT ")
+            My.Application.Log.WriteEntry("POP3: " & StatResp)
+
+            'Get Messages count
+            server_Stat = StatResp.Split(" ")
+            My.Application.Log.WriteEntry("POP3 Message count: " & server_Stat(1))
+            ret_Val = 1
+        End If
+    End Sub
+
+    Sub CloseServer()
+        StatResp = Login(m_sslStream, "QUIT ")
+        My.Application.Log.WriteEntry("POP3: " & StatResp)
+        pClient.Close()
+        ret_Val = 0
+    End Sub
 
     Sub Disable()
         My.Application.Log.WriteEntry("Unloading mail module")
@@ -66,6 +119,8 @@ Module modMail
             oClient.UseDefaultCredentials = False
             oClient.Credentials = New System.Net.NetworkCredential(My.Settings.Mail_Username, My.Settings.Mail_Password)
             oClient.DeliveryMethod = SmtpDeliveryMethod.Network
+
+            CheckMail()
         Else
             My.Application.Log.WriteEntry("Mail module is disabled, module not loaded")
         End If
@@ -94,4 +149,14 @@ Module modMail
     Sub Unload()
 
     End Sub
+
+    Function Login(ByVal SslStrem As SslStream, ByVal Server_Command As String) As String
+        Dim Read_Stream2 = New StreamReader(SslStrem)
+        Server_Command = Server_Command + vbCrLf
+        m_buffer = System.Text.Encoding.ASCII.GetBytes(Server_Command.ToCharArray())
+        m_sslStream.Write(m_buffer, 0, m_buffer.Length)
+        Dim Server_Response As String
+        Server_Response = Read_Stream2.ReadLine()
+        Return Server_Response
+    End Function
 End Module

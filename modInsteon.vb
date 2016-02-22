c846b9c1d61caedd6820d1f0507a269749ff7802
Jacob Weisz <ocdtrekkie@gmail.com>
2016-02-21 23:25:16 -0600

    Rest of redundant Insteon send code eliminated

--- modInsteon.vb
+++ modInsteon.vb
@@ -94,7 +94,6 @@ Module modInsteon
     Sub InsteonAlarmControl(ByVal strAddress As String, ByRef ResponseMsg As String, ByVal Command1 As String, Optional ByVal intSeconds As Integer = 0)
         Dim comm1 As Short
         Dim comm2 As Short
-        Dim data(7) As Byte
 
         ' TODO: Yes, I'm currently ignoring strAddress here, this is terrible. I should do better.
         If My.Settings.Insteon_AlarmAddr = "" Then
@@ -102,8 +101,6 @@ Module modInsteon
             My.Settings.Insteon_AlarmAddr = InputBox("Enter Alarm Address", "Alarm")
         End If
         strAddress = My.Settings.Insteon_AlarmAddr
-
-        Dim arrAddress() As String = strAddress.Split(".")
 
         Select Case Command1
             Case "Off", "off"
@@ -122,20 +119,7 @@ Module modInsteon
                 Exit Sub
         End Select
 
-        data(0) = 2 'all commands start with 2
-        data(1) = 98 '0x62 = the PLM command to send an Insteon standard or extended message
-        data(2) = Convert.ToInt32(arrAddress(0), 16) 'three byte address of device
-        data(3) = Convert.ToInt32(arrAddress(1), 16)
-        data(4) = Convert.ToInt32(arrAddress(2), 16)
-        data(5) = 15 'flags
-        data(6) = comm1
-        data(7) = comm2
-        Try
-            SerialPLM.Write(data, 0, 8)
-        Catch Excep As System.InvalidOperationException
-            My.Application.Log.WriteException(Excep)
-            ResponseMsg = "ERROR: " + Excep.Message
-        End Try
+        InsteonSendStdCommand(strAddress, comm1, comm2)
 
         ' Shut it back off
         If (Command1 = "On" Or Command1 = "on") And intSeconds > 0 Then
@@ -178,44 +162,13 @@ Module modInsteon
     End Sub
 
     Sub InsteonProductDataRequest(ByVal strAddress As String, ByRef ResponseMsg As String, ByVal EngineVersion As Short)
-        Dim arrAddress() As String = strAddress.Split(".")
-
         Select Case EngineVersion
             Case 0 Or 1
-                Dim data(7) As Byte
-                data(0) = 2 'all commands start with 2
-                data(1) = 98 '0x62 = the PLM command to send an Insteon standard or extended message
-                data(2) = Convert.ToInt32(arrAddress(0), 16) 'three byte address of device
-                data(3) = Convert.ToInt32(arrAddress(1), 16)
-                data(4) = Convert.ToInt32(arrAddress(2), 16)
-                data(5) = 15 'flags
-                data(6) = 3 ' Product Data Request
-                data(7) = 0
-                Try
-                    SerialPLM.Write(data, 0, 8)
-                Catch Excep As System.InvalidOperationException
-                    My.Application.Log.WriteException(Excep)
-                    ResponseMsg = "ERROR: " + Excep.Message
-                End Try
+                InsteonSendStdCommand(strAddress, 3, 0)
             Case 2
-                Dim data(21) As Byte
-                data(0) = 2 'all commands start with 2
-                data(1) = 98 '0x62 = the PLM command to send an Insteon standard or extended message
-                data(2) = Convert.ToInt32(arrAddress(0), 16) 'three byte address of device
-                data(3) = Convert.ToInt32(arrAddress(1), 16)
-                data(4) = Convert.ToInt32(arrAddress(2), 16)
-                data(5) = 31 'flags
-                data(6) = 16 ' ID Request
-                data(7) = 0
-                data(21) = (Not (data(6) + data(7))) + 1
-                Try
-                    SerialPLM.Write(data, 0, 22)
-                Catch Excep As System.InvalidOperationException
-                    My.Application.Log.WriteException(Excep)
-                    ResponseMsg = "ERROR: " + Excep.Message
-                End Try
+                ' I have no evidence this actually helps or works
+                InsteonSendExtCommand(strAddress, 16, 0)
         End Select
-
     End Sub
 
     Sub InsteonSendExtCommand(ByVal strAddress As String, ByVal comm1 As Short, ByVal comm2 As Short)

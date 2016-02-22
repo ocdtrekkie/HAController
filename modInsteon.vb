ffcd2a34be14a2cdd28306c51b4dfd627b4aee78
Jacob Weisz <ocdtrekkie@gmail.com>
2016-02-21 20:53:00 -0600

    Create standardized Insteon send functions
    I'm gonna shorten up this module a lot with this.

--- modInsteon.vb
+++ modInsteon.vb
@@ -249,6 +249,45 @@ Module modInsteon
 
     End Sub
 
+    Sub InsteonSendExtCommand(ByVal strAddress As String, ByVal comm1 As Short, ByVal comm2 As Short)
+        Dim data(21) As Byte
+        Dim arrAddress() As String = strAddress.Split(".")
+
+        data(0) = 2 'all commands start with 2
+        data(1) = 98 '0x62 = the PLM command to send an Insteon standard or extended message
+        data(2) = Convert.ToInt32(arrAddress(0), 16) 'three byte address of device
+        data(3) = Convert.ToInt32(arrAddress(1), 16)
+        data(4) = Convert.ToInt32(arrAddress(2), 16)
+        data(5) = 31 'flags
+        data(6) = comm1
+        data(7) = comm2
+        data(21) = (Not (data(6) + data(7))) + 1
+        Try
+            SerialPLM.Write(data, 0, 22)
+        Catch Excep As System.InvalidOperationException
+            My.Application.Log.WriteException(Excep)
+        End Try
+    End Sub
+
+    Sub InsteonSendStdCommand(ByVal strAddress As String, ByVal comm1 As Short, ByVal comm2 As Short)
+        Dim data(7) As Byte
+        Dim arrAddress() As String = strAddress.Split(".")
+
+        data(0) = 2 'all commands start with 2
+        data(1) = 98 '0x62 = the PLM command to send an Insteon standard or extended message
+        data(2) = Convert.ToInt32(arrAddress(0), 16) 'three byte address of device
+        data(3) = Convert.ToInt32(arrAddress(1), 16)
+        data(4) = Convert.ToInt32(arrAddress(2), 16)
+        data(5) = 15 'flags
+        data(6) = comm1
+        data(7) = comm2
+        Try
+            SerialPLM.Write(data, 0, 8)
+        Catch Excep As System.InvalidOperationException
+            My.Application.Log.WriteException(Excep)
+        End Try
+    End Sub
+
     Sub InsteonThermostatControl(ByVal strAddress As String, ByRef ResponseMsg As String, ByVal Command1 As String, Optional ByVal intTemperature As Integer = 72)
         Dim comm1 As Short
         Dim comm2 As Short

df1569b2cb50cbda936a1582e7503a76c29c8f29
Jacob Weisz <ocdtrekkie@gmail.com>
2016-02-25 20:12:08 -0600

    Get process list
    Probably will use this as a reference for killing processes, but we'll
    see!

--- modComputer.vb
+++ modComputer.vb
@@ -9,5 +9,18 @@ Module modComputer
         Dim ramSize As Integer = My.Computer.Info.TotalPhysicalMemory / 1024 / 1024
         My.Application.Log.WriteEntry("Memory: " & ramSize & " MB RAM")
         My.Application.Log.WriteEntry("Screen: " & My.Computer.Screen.Bounds.Width & " x " & My.Computer.Screen.Bounds.Height)
+
+        GetProcesses()
     End Sub
+
+    Function GetProcesses() As String
+        Dim ProcessList As String = ""
+
+        For Each p As Process In Process.GetProcesses
+            ProcessList = ProcessList & p.ProcessName & ", "
+        Next
+
+        My.Application.Log.WriteEntry("Running Processes: " & ProcessList)
+        Return ProcessList
+    End Function
 End Module

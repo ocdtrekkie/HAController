0a829bb8350f6d51799a264341a8d19a296e9a75
Jacob Weisz <ocdtrekkie@gmail.com>
2017-07-20 11:57:47 -0500

    Don't divide by zero
    lol

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -642,7 +642,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("0")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("1")>  _
         Public Property GPS_RateLimit() As Integer
             Get
                 Return CType(Me("GPS_RateLimit"),Integer)

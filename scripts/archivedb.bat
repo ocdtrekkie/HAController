5b17cd6921d514a964dc89516a2ec3636d12328a
Jacob Weisz <ocdtrekkie@gmail.com>
2017-08-08 18:35:43 -0500

    Archive scripts wait until resources are released
    Five seconds might actually be excessive here.

--- scripts/archivedb.bat
+++ scripts/archivedb.bat
@@ -1,6 +1,7 @@
 rem https://stackoverflow.com/questions/18895422/how-to-use-a-batch-file-to-rename-a-file-to-include-the-date/18907550#18907550
 
 taskkill /im HAController.exe
+timeout /t 5 /nobreak
 
 for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set "dt=%%a"
 set "YY=%dt:~2,2%"

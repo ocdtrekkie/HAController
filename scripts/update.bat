22e9317d02f4799633bbd29886ad22160979f91d
Jacob Weisz <inbox@jacobweisz.com>
2017-08-14 14:50:33 -0500

    Change update server location

--- scripts/update.bat
+++ scripts/update.bat
@@ -1,5 +1,5 @@
 taskkill /im HAController.exe
-C:\HAC\scripts\wget.exe -O C:\HAC\scripts\update.zip http://files.ocdtrekkie.com/dl/hac/update.zip
+C:\HAC\scripts\wget.exe -O C:\HAC\scripts\update.zip https://hac.jacobweisz.com/dl/update.zip
 C:\HAC\scripts\7z.exe x -aoa -oC:\HAC\scripts\temp C:\HAC\scripts\update.zip
 del /q C:\HAC\scripts\update.zip
 robocopy C:\HAC\scripts\temp C:\HAC /E

ba810b4385fb0194a2a31caddd9f30108d0ccbbb
Jacob Weisz <ocdtrekkie@gmail.com>
2017-08-14 18:16:13 -0500

    Replace updateupdate script
    Uses new update location, isn't as stupid

--- scripts/updateupdate.bat
+++ scripts/updateupdate.bat
@@ -1,2 +1,6 @@
-C:\HAC\scripts\wget.exe -O C:\HAC\scripts\update.bat http://files.ocdtrekkie.com/dl/hac/update.bat
+C:\HAC\scripts\wget.exe -O C:\HAC\scripts\updateupdate.zip https://hac.jacobweisz.com/dl/updateupdate.zip
+C:\HAC\scripts\7z.exe x -aoa -oC:\HAC\scripts\temp C:\HAC\scripts\updateupdate.zip
+del /q C:\HAC\scripts\updateupdate.zip
+robocopy C:\HAC\scripts\temp C:\HAC\scripts /E
+rmdir /s /q C:\HAC\scripts\temp
 exit

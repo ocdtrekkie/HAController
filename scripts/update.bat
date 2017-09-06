d362910abd644e7c1ff1e0c761671a9a616e2af5
Jacob Weisz <ocdtrekkie@gmail.com>
2017-09-05 23:37:08 -0500

    Set retries on update script robocopy

--- scripts/update.bat
+++ scripts/update.bat
@@ -2,7 +2,7 @@ taskkill /im HAController.exe
 C:\HAC\scripts\wget.exe -O C:\HAC\scripts\update.zip https://hac.jacobweisz.com/dl/update.zip
 C:\HAC\scripts\7z.exe x -aoa -oC:\HAC\scripts\temp C:\HAC\scripts\update.zip
 del /q C:\HAC\scripts\update.zip
-robocopy C:\HAC\scripts\temp C:\HAC /E
+robocopy C:\HAC\scripts\temp C:\HAC /E /R:6 /W:5
 rmdir /s /q C:\HAC\scripts\temp
 powershell -c echo `a
 cd C:\HAC

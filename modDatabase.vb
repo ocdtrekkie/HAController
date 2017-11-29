5bd79692bed161c4d9bf57c6bd347a8568b94a34
Jacob Weisz <inbox@jacobweisz.com>
2017-11-28 20:29:42 -0600

    Fix Places table creation
    Also remove extraneous space

--- modDatabase.vb
+++ modDatabase.vb
@@ -9,7 +9,7 @@ Module modDatabase
         Execute("CREATE TABLE IF NOT EXISTS DEVICES(Id INTEGER PRIMARY KEY, Name TEXT, Type TEXT, Model TEXT, Location TEXT, Address TEXT UNIQUE)")
         Execute("CREATE TABLE IF NOT EXISTS ENVIRONMENT(Id INTEGER PRIMARY KEY, Date TEXT, Source TEXT, Location TEXT, Temperature INTEGER, Humidity INTEGER, Condition TEXT)")
         Execute("CREATE TABLE IF NOT EXISTS LOCATION(Id INTEGER PRIMARY KEY, Date TEXT, Latitude REAL, Longitude REAL, Speed REAL)")
-        Execute("CREATE TABLE IF NOT EXISTS PLACES(Id INTEGER PRIMARY KEY, Date TEXT, Name STRING, Location STRING)")
+        Execute("CREATE TABLE IF NOT EXISTS PLACES(Id INTEGER PRIMARY KEY, Date TEXT, Name TEXT, Location TEXT)")
     End Sub
 
     Sub Execute(query As String)

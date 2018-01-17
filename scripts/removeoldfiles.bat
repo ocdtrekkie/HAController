87147fa7e2da4da5c75d889a404f13acf75db1ba
Jacob Weisz <ocdtrekkie@gmail.com>
2018-01-16 19:22:40 -0600

    Add script to remove old components

---
+++ scripts/removeoldfiles.bat
@@ -0,0 +1,8 @@
+del /q C:\HAC\Common.Logging.Core.dll
+del /q C:\HAC\Common.Logging.Core.pdb
+del /q C:\HAC\Common.Logging.Core.xml
+del /q C:\HAC\Common.Logging.dll
+del /q C:\HAC\Common.Logging.pdb
+del /q C:\HAC\Common.Logging.xml
+powershell -c echo `a
+exit

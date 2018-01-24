a37052c0493fcd0691636c185fee14a171c59a56
Jacob Weisz <ocdtrekkie@gmail.com>
2018-01-23 20:12:45 -0600

    Check nicknames for cleanliness

--- modPersons.vb
+++ modPersons.vb
@@ -2,15 +2,18 @@
 
 Module modPersons
     Sub AddPersonDb(ByVal strNickname As String, Optional ByVal PersonType As Integer = 0)
-        If CheckDbForPerson(strNickname) = 0 Then
+        If modDatabase.IsCleanString(strNickname) AndAlso CheckDbForPerson(strNickname) = 0 Then
             modDatabase.Execute("INSERT INTO PERSONS (Date, Nickname, PersonType) VALUES('" + Now.ToUniversalTime.ToString("u") + "', '" + strNickname + "', '" + CStr(PersonType) + "')")
         End If
     End Sub
 
     Function CheckDbForPerson(ByVal strNickname As String) As Integer
         Dim result As Integer = New Integer
-
-        modDatabase.ExecuteScalar("SELECT Id FROM PERSONS WHERE Nickname = """ + strNickname + """", result)
+        If modDatabase.IsCleanString(strNickname) Then
+            modDatabase.ExecuteScalar("SELECT Id FROM PERSONS WHERE Nickname = """ + strNickname + """", result)
+        Else
+            My.Application.Log.WriteEntry(strNickname + " is not a valid query", TraceEventType.Warning)
+        End If
         If result <> 0 Then
             My.Application.Log.WriteEntry(strNickname + " database ID is " + result.ToString)
             Return result

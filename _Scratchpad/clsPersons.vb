107e90e0b4789494f7e213db7167e4a07a6c6f0a
Jacob Weisz <inbox@jacobweisz.com>
2018-01-23 21:11:47 -0600

    Lowercase enums, add MsgPreferences to design

--- _Scratchpad/clsPersons.vb
+++ _Scratchpad/clsPersons.vb
@@ -8,29 +8,36 @@ Class HAUser
     Public Property Email As String
 	Public Property PhoneNum As String
 	Public Property PhoneCarrier As PhoneCarriers 'Used for emailing cell phones, like @vtext.com for Verizon
+	Public Property MsgPreference As MsgPreferences
 	Public Property Address As String
     Public Property Gender As Genders
     Public Property IsSocial As Boolean 'Use friendlier responses
 End Class
 
 Public Enum Genders As Integer
-	Unknown = 0 'Avoid pronoun use
-	Male = 1 'Identifies as he/him
-	Female = 2 'Identifies as she/her
-	Nonbinary = 3 'Avoid pronoun use
+	unknown = 0 'Avoid pronoun use
+	male = 1 'Identifies as he/him
+	female = 2 'Identifies as she/her
+	nonbinary = 3 'Avoid pronoun use
 End Enum
 
 Public Enum PersonTypes As Integer
-	Contact = 0 'Can be contacted
-	Guest = 1 'Can ask questions
-	User = 2 'Can issue commands
-	Admin = 4 'Primary user
+	contact = 0 'Can be contacted
+	guest = 1 'Can ask questions
+	user = 2 'Can issue commands
+	admin = 4 'Primary user
 End Enum
 
 Public Enum PhoneCarriers As Integer
-	Unknown = 0
-	Verizon = 1
-	ATT = 2
-	Sprint = 3
-	TMobile = 4
+	unknown = 0
+	verizon = 1
+	att = 2
+	sprint = 3
+	tmobile = 4
+End Enum
+
+Public Enum MsgPreferences As Integer
+	unknown = 0
+	email = 1
+	text = 2
 End Enum

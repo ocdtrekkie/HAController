b51067dbcebff3e0949350aec1b054e8cd09bc76
Jacob Weisz <inbox@jacobweisz.com>
2018-04-02 23:58:46 -0500

    Update removeoldfiles script to clean Quartz

--- scripts/removeoldfiles.bat
+++ scripts/removeoldfiles.bat
@@ -4,5 +4,9 @@ del /q C:\HAC\Common.Logging.Core.xml
 del /q C:\HAC\Common.Logging.dll
 del /q C:\HAC\Common.Logging.pdb
 del /q C:\HAC\Common.Logging.xml
+del /q C:\HAC\quartz.config
+del /q C:\HAC\Quartz.dll
+del /q C:\HAC\Quartz.pdb
+del /q C:\HAC\Quartz.xml
 powershell -c echo `a
 exit

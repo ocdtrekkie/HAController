7251f547e2288e002cd02884ec91aee76b0d1bf7
Jacob Weisz <inbox@jacobweisz.com>
2018-12-19 23:01:14 -0600

    Update removeoldfiles script

--- scripts/removeoldfiles.bat
+++ scripts/removeoldfiles.bat
@@ -4,9 +4,15 @@ del /q C:\HAC\Common.Logging.Core.xml
 del /q C:\HAC\Common.Logging.dll
 del /q C:\HAC\Common.Logging.pdb
 del /q C:\HAC\Common.Logging.xml
+del /q C:\HAC\EntityFramework.dll
+del /q C:\HAC\EntityFramework.SqlServer.dll
+del /q C:\HAC\EntityFramework.SqlServer.xml
+del /q C:\HAC\EntityFramework.xml
 del /q C:\HAC\quartz.config
 del /q C:\HAC\Quartz.dll
 del /q C:\HAC\Quartz.pdb
 del /q C:\HAC\Quartz.xml
+del /q C:\HAC\System.Data.SQLite.EF6.dll
+del /q C:\HAC\System.Data.SQLite.Linq.dll
 powershell -c echo `a
 exit

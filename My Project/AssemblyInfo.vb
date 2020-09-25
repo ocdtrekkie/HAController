b9b8dfb0173d8301e6f3f229c5a19ecae347c888
Jacob Weisz <inbox@jacobweisz.com>
2020-09-24 20:04:37 -0500

    Version 1.0.0.50

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.49")>
-<Assembly: AssemblyFileVersion("1.0.0.49")>
+<Assembly: AssemblyVersion("1.0.0.50")>
+<Assembly: AssemblyFileVersion("1.0.0.50")>

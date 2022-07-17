2571a71032506e264627a59de6524586885927b5
Jacob Weisz <inbox@jacobweisz.com>
2022-07-17 01:36:59 -0500

    Sync: Send public IP address in URL

--- modSync.vb
+++ modSync.vb
@@ -63,7 +63,7 @@
             If modGlobal.IsOnline = True Then
                 My.Application.Log.WriteEntry("Sending " & strMessageType & " to " & strDestination, TraceEventType.Verbose)
                 Dim Req As System.Net.HttpWebRequest
-                Dim TargetUri As New Uri(My.Settings.Sync_ServerURL & "?message_type=" & strMessageType & "&destination=" & strDestination & "&access_key=" & My.Settings.Sync_AccessKey & "&message=" & strMessage & "&user_agent=HAController/" & My.Application.Info.Version.ToString)
+                Dim TargetUri As New Uri(My.Settings.Sync_ServerURL & "?message_type=" & strMessageType & "&destination=" & strDestination & "&access_key=" & My.Settings.Sync_AccessKey & "&message=" & strMessage & "&user_agent=HAController/" & My.Application.Info.Version.ToString & "&ip_address=" & My.Settings.Ping_LastKnownPublicIP)
                 Dim Output As System.Net.HttpWebResponse
                 Req = DirectCast(System.Net.HttpWebRequest.Create(TargetUri), System.Net.HttpWebRequest)
                 Req.UserAgent = "HAController/" & My.Application.Info.Version.ToString

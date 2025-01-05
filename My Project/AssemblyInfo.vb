96772891fe65d2c4c1e8102e2d7f25ee526279ec
Jacob Weisz <inbox@jacobweisz.com>
2025-01-05 13:47:04 -0600

    Version 1.0.0.77

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("Jacob Weisz")> 
 <Assembly: AssemblyProduct("HAController")>
-<Assembly: AssemblyCopyright("Copyright ©  2015-2024 Jacob Weisz")>
+<Assembly: AssemblyCopyright("Copyright ©  2015-2025 Jacob Weisz")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.76")>
-<Assembly: AssemblyFileVersion("1.0.0.76")>
+<Assembly: AssemblyVersion("1.0.0.77")>
+<Assembly: AssemblyFileVersion("1.0.0.77")>
